2eb8148fc813926b084899c236f777c94fffdab3
Kevin <862860000@qq.com>
2018-04-24 15:38:55 +0800

    date UTC+8.

--- src/Tiger.WebApi.Core/Result/ResultCode.cs
+++ src/Tiger.WebApi.Core/Result/ResultCode.cs
@@ -2,25 +2,27 @@
 
 namespace Tiger.WebApi.Core.Result
 {
-    public enum ResultCode
-    {
-        [ResultCodeMessage("Header错误")]
-        HeaderError = 0,
-        [ResultCodeMessage("")]
-        Success = 1,
-        [ResultCodeMessage("签名不正确")]
-        SignError = 2,
-        [ResultCodeMessage("API未找到")]
-        MethodNotFound = 3,
-        [ResultCodeMessage("请求超时")]
-        Timestamp = 4,
-        [ResultCodeMessage("时间戳格式错误")]
-        TimeFormat = 5,
-        [ResultCodeMessage("AppKey错误")]
-        AppKeyNotFound = 6,
-        [ResultCodeMessage("请求参数不完整")]
-        RequestParameterIncomplete = 7,
-        [ResultCodeMessage("接口请求出错")]
-        MethodError = 8
-    }
+	public enum ResultCode
+	{
+		[ResultCodeMessage("Header错误")]
+		HeaderError = 0,
+		[ResultCodeMessage("")]
+		Success = 1,
+		[ResultCodeMessage("签名不正确")]
+		SignError = 2,
+		[ResultCodeMessage("API未找到")]
+		MethodNotFound = 3,
+		[ResultCodeMessage("请求超时")]
+		Timestamp = 4,
+		[ResultCodeMessage("时间戳格式错误")]
+		TimeFormat = 5,
+		[ResultCodeMessage("AppKey错误")]
+		AppKeyNotFound = 6,
+		[ResultCodeMessage("请求参数不完整")]
+		RequestParameterIncomplete = 7,
+		[ResultCodeMessage("接口请求出错")]
+		MethodError = 8,
+		[ResultCodeMessage("自定义错误")]
+		CustomError = 9
+	}
 }
